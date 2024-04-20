cde6acd91effef5c00c7c755d617e4b38405c0be
Marcos Gabriel de Oliveira Favaretto <marcosfavaretto.dev@gmail.com>
2024-04-20 20:40:55 +0000

    :books:DOCS: Adds some annotations to MyFirstProgram

---
+++ MyFirstProgram.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;        // creates a namesapce so that "std::" is not necessary.
+
+int main()
+{
+    cout << "Hello World!"; // "cout" means character output device (screen).
+    return 0;
+}
